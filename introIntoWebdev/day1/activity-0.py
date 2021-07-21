935ec3d517b63f60436ac896e878c6df01e225a5
Dakota Newman <dakota.newman@yahoo.com>
2021-07-21 12:48:54 -0700

    day one - html practice

---
+++ introIntoWebdev/day1/activity-0.py
@@ -0,0 +1,2 @@
+# Think of your favorite website and find out more about their data center
+# Hulu - AWS is the infrastructure it uses, built with Ruby on Rails, new renewable energy facility in Las Vegas
